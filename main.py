97eb9cf70b29c9c589a3b0c5e369056a76cdee51
FEPSFY6845340 <bg00629@surrey.ac.uk>
2025-06-19 00:35:06 +0100

    Day 32: Intial commit

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
 import smtplib
 
 my_email = "bijay765@gmail.com"
+password = "placeholder"
 
 connection = smtplib.SMTP("smtp.gmail.com")
-connection.starttls()
-
+connection.starttls() #ENCRYPTS CONNECTION
+connection.login(user=my_email, password= password)
+connection.sendmail(from_addr=my_email, to_addrs="765crimson@gmail.com", msg="Hello")
+connection.close()
