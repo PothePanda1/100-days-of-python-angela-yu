75bb732cd20c0b8499945adb6b729dc51c212907
FEPSFY6845340 <bg00629@surrey.ac.uk>
2025-06-19 01:29:22 +0100

    Day 32: Intial commit

--- main.py
+++ main.py
@@ -1,11 +1,26 @@
 import smtplib
+import datetime as dt
+import random
 
 my_email = "bijay765@gmail.com"
-password = "placeholder"
+password = "rbzhbtpoqldsjcuh"
+target_email = "765crimson@gmail.com"
 
-connection = smtplib.SMTP("smtp.gmail.com")
-connection.starttls() #ENCRYPTS CONNECTION
-connection.login(user=my_email, password= password)
-connection.sendmail(from_addr=my_email, to_addrs="765crimson@gmail.com", msg="Hello")
-connection.close()
-ss
+now = dt.datetime.now()
+day_of_the_week = now.weekday()
+
+if day_of_the_week == 3:
+
+    with open("quotes.txt") as f:
+        all_quotes = f.readlines()
+        quote = random.choice(all_quotes)
+        print(quote)
+
+    with smtplib.SMTP("smtp.gmail.com") as connection:
+        connection.starttls() #ENCRYPTS CONNECTION
+        connection.login(user=my_email, password= password)
+        connection.sendmail(
+            from_addr=my_email,
+            to_addrs=target_email,
+            msg=f"Subject:Monday Motivator\n\n{quote}"
+        )
